248ebe085251c8a0ef6c5a47e0aec0bdae1d867c
mabahandulla <40692963+mabahandulla@users.noreply.github.com>
2019-03-21 22:13:51 +0300

    Just for fun

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,14 +1,25 @@
 (function(window){
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+/*
+	I wrote some code that displays the result directly to the page.
+	Just for fun :)
+*/
+const divContainer = document.querySelector('.output');
 
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === "j") {
-    byeSpeaker.speak(names[i]);
+    byeSpeaker.speak(names[i]); //printing to console
+	
+    divContainer.appendChild(document.createElement('p'));
+    divContainer.lastElementChild.textContent = "Good bye " + names[i]; //printing to page
   } else {
-    helloSpeaker.speak(names[i]);
+    helloSpeaker.speak(names[i]); //printing to console
+
+    divContainer.appendChild(document.createElement('p'));
+    divContainer.lastElementChild.textContent = "Hello " + names[i]; //printing to page
   }
 }
 
